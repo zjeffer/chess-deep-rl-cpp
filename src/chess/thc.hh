4e2823b0dde4d066366cbb305e18ae4fc1f4653c
zjeffer <4633209+zjeffer@users.noreply.github.com>
2022-02-06 20:34:32 +0100

    removed unnecessary lines in shared chess lib => greatly minimized size of thc.cc

--- src/chess/thc.hh
+++ src/chess/thc.hh
@@ -16,7 +16,6 @@
         ChessPositionRaw.h
         ChessPosition.h
         ChessRules.h
-        ChessEvaluation.h
 
  */
 
@@ -202,6 +201,10 @@ public:
     bool operator !=(const Move &other) const
     {
         return( *((int32_t *)this) != *((int32_t *)(&other)) );
+    }
+
+    bool operator <(const Move &other) const {
+        return this->src < other.src;
     }
 
     // Use these sparingly when you need to specifically mark
@@ -653,68 +656,3 @@ protected:
 } //namespace thc
 
 #endif //CHESSRULES_H
-/****************************************************************************
- * ChessEvaluation.h Chess classes - Simple chess AI, leaf scoring function for position
- *  Author:  Bill Forster
- *  License: MIT license. Full text of license is in associated file LICENSE
- *  Copyright 2010-2020, Bill Forster <billforsternz at gmail dot com>
- ****************************************************************************/
-#ifndef CHESSEVALUATION_H
-#define CHESSEVALUATION_H
-
-// TripleHappyChess
-namespace thc
-{
-
-class ChessEvaluation: public ChessRules
-{
-public:
-    // Default contructor
-    ChessEvaluation() : ChessRules()
-    {
-    }
-
-    // Copy constructor
-    ChessEvaluation( const ChessPosition& src ) : ChessRules( src )
-    {
-    }
-
-    // Assignment operator
-    ChessEvaluation& operator=( const ChessPosition& src )
-    {
-        *((ChessRules *)this) = src;
-        return *this;
-    }
-
-
-    // Use leaf evaluator to generate a sorted move list
-    void GenLegalMoveListSorted( MOVELIST *list );
-    void GenLegalMoveListSorted( std::vector<Move> &moves );
-
-    // Evaluate a position, leaf node (useful for playing programs)
-    void EvaluateLeaf( int &material, int &positional );
-
-// internal stuff
-protected:
-
-    // Always some planning before calculating a move
-    void Planning();
-
-    // Calculate material that side to play can win directly
-    int Enprise();
-    int EnpriseWhite();   // fast white to move version
-    int EnpriseBlack();   // fast black to move version
-
-// misc
-private:
-    bool white_is_better;
-    bool black_is_better;
-    int  planning_score_white_pieces;
-    int  planning_score_black_pieces;
-    int  planning_white_piece_pawn_percent;
-    int  planning_black_piece_pawn_percent;
-};
-
-} //namespace thc
-
-#endif //CHESSEVALUATION_H
